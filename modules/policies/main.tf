94a6c3bed736d01fba9b0d292d5c7d35853bda4d
pbean <paulbeanjr@gmail.com>
2025-07-09 16:37:55 -0700

    Trying new data source

--- modules/policies/main.tf
+++ modules/policies/main.tf
@@ -9,80 +9,58 @@ terraform {
 
 # --- Sign On Policies ---
 
-data "okta_policy" "signon_source" {
-  provider = okta.preview
+data "okta_policy_signon" "signon_source" {
   for_each = toset(var.signon_policy_names)
-  name     = each.key
-  type     = "OKTA_SIGN_ON"
+  provider = okta.preview
+  name     = each.value
 }
 
 resource "okta_policy_signon" "imported_signon" {
+  for_each    = { for k, v in data.okta_policy_signon.signon_source : k => v if v.status == "ACTIVE" }
   provider    = okta.production
-  for_each    = { for k, v in data.okta_policy.signon_source : k => v if v != null }
   name        = "oktapreview-${each.value.name}"
   status      = "INACTIVE"
   description = each.value.description
 }
 
-# --- Password Policies ---
-
-data "okta_policy" "password_source" {
-  provider = okta.preview
+data "okta_policy_password" "password_source" {
   for_each = toset(var.password_policy_names)
-  name     = each.key
-  type     = "PASSWORD"
+  provider = okta.preview
+  name     = each.value
 }
 
 resource "okta_policy_password" "imported_password" {
-  provider                          = okta.production
-  for_each                          = { for k, v in data.okta_policy.password_source : k => v if v != null }
-  name                              = "oktapreview-${each.value.name}"
-  status                            = "INACTIVE"
-  description                       = each.value.description
-  priority                          = each.value.priority
-  password_min_length               = each.value.password_min_length
-  password_min_lowercase            = each.value.password_min_lowercase
-  password_min_number               = each.value.password_min_number
-  password_min_symbol               = each.value.password_min_symbol
-  password_min_uppercase            = each.value.password_min_uppercase
-  password_max_age_days             = each.value.password_max_age_days
-  password_expire_warn_days         = each.value.password_expire_warn_days
-  password_history_count            = each.value.password_history_count
-  password_max_lockout_attempts     = each.value.password_max_lockout_attempts
-  password_auto_unlock_minutes      = each.value.password_auto_unlock_minutes
-  password_show_lockout_failures    = each.value.password_show_lockout_failures
-  password_lockout_notification_channels = each.value.password_lockout_notification_channels
+  for_each                               = { for k, v in data.okta_policy_password.password_source : k => v if v.status == "ACTIVE" }
+  provider                               = okta.production
+  name                                   = "oktapreview-${each.value.name}"
+  status                                 = "INACTIVE"
+  description                            = each.value.description
+  priority                               = each.value.priority
+  password_min_length                    = each.value.min_length
+  password_min_lowercase                 = each.value.min_lowercase
+  password_min_number                    = each.value.min_number
+  password_min_symbol                    = each.value.min_symbol
+  password_min_uppercase                 = each.value.min_uppercase
+  password_max_age_days                  = each.value.max_age_days
+  password_expire_warn_days              = each.value.expire_warn_days
+  password_history_count                 = each.value.history_count
+  password_max_lockout_attempts          = each.value.max_lockout_attempts
+  password_auto_unlock_minutes           = each.value.auto_unlock_minutes
+  password_show_lockout_failures         = each.value.show_lockout_failures
+  password_lockout_notification_channels = each.value.lockout_notification_channels
 }
 
-# --- MFA Policies ---
-
-data "okta_policy" "mfa_source" {
-  provider = okta.preview
+data "okta_policy_mfa" "mfa_source" {
   for_each = toset(var.mfa_policy_names)
-  name     = each.key
-  type     = "MFA_ENROLLMENT"
+  provider = okta.preview
+  name     = each.value
 }
 
 resource "okta_policy_mfa" "imported_mfa" {
-  provider      = okta.production
-  for_each      = { for k, v in data.okta_policy.mfa_source : k => v if v != null }
-  name          = "oktapreview-${each.value.name}"
-  status        = "INACTIVE"
-  description   = each.value.description
-  priority      = each.value.priority
-  okta_password = each.value.okta_password
-  okta_otp      = each.value.okta_otp
-  okta_question = each.value.okta_question
-  okta_sms      = each.value.okta_sms
-  okta_email    = each.value.okta_email
-  okta_call     = each.value.okta_call
-  okta_verify   = each.value.okta_verify
-  fido_u2f      = each.value.fido_u2f
-  fido_webauthn = each.value.fido_webauthn
-  google_otp    = each.value.google_otp
-  security_question = each.value.security_question
-  symantec_vip  = each.value.symantec_vip
-  duo           = each.value.duo
-  yubikey_token = each.value.yubikey_token
-  external_idp  = each.value.external_idp
+  for_each       = { for k, v in data.okta_policy_mfa.mfa_source : k => v if v.status == "ACTIVE" }
+  provider       = okta.production
+  name           = "oktapreview-${each.value.name}"
+  status         = "INACTIVE"
+  description    = each.value.description
+  authenticators = each.value.authenticators
 }
